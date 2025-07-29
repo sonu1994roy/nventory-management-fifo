1d70fb1ad0a1bfd910088ad8f19f5ba95d499784
sonu6492 <sonu@teminos.com>
2025-07-29 18:17:55 +0530

    Initial commit from local frontend

--- backend/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { login } = require("../controllers/auth.controller");
-const router = express.Router();
-
-router.post("/login", login);
-
-module.exports = router;
